e1ef13dd82d03ce211f6c06939f6a1b3c8480fcf
qkh <myshengun@163.com>
2022-06-02 17:49:11 +0800

    unicafe

--- part1/courseinfo/src/App.js
+++ part1/courseinfo/src/App.js
@@ -5,27 +5,33 @@ import Content from "./components/content/Content";
 import Total from "./components/total/Total";
 
 const App = () => {
-  const course = "Half Stack application development";
+  const course = {
+    name: "Half Stack application development",
+    parts: [
+      {
+        name: "Fundamentals of React",
+        exercises: 10,
+      },
+      {
+        name: "Using props to pass data",
+        exercises: 7,
+      },
+      {
+        name: "State of a component",
+        exercises: 14,
+      },
+    ],
+  };
 
-  const courses = [
-    { part: "Fundamentals of React", exercises: 10 },
-    { part: "Using props to pass data", exercises: 7 },
-    { part: "State of a component", exercises: 14 },
-  ];
-
-  const renderedContent = courses.map((item) => (
-    <Content part={item.part} exercises={item.exercises} />
-  ));
-
-  const total = courses.reduce((pre, item) => {
+  const total = course.parts.reduce((pre, item) => {
     pre += item.exercises;
     return pre;
   }, 0);
 
   return (
     <div>
-      <Header course={course} />
-      {renderedContent}
+      <Header course={course.name} />
+      <Content courses={course.parts} />
       <Total total={total} />
     </div>
   );
--- part1/courseinfo/src/components/content/Content.js
+++ part1/courseinfo/src/components/content/Content.js
@@ -1,9 +1,10 @@
-const Content = ({ part, exercises }) => {
-  return (
-    <p>
-      {part} {exercises}
-    </p>
-  );
+import Part from "./Part";
+
+const Content = ({ courses }) => {
+  const renderedPart = courses.map((item) => (
+    <Part part={item.part} exercises={item.exercises} />
+  ));
+  return <p>{renderedPart}</p>;
 };
 
 export default Content;
